b42af3bde9963f7f9cb84ab675960c51254d28b4
Isha <isha2002.shinde@gmail.com>
2026-06-29 23:06:23 +0530

    Fix Groq client initialization

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
 from flask import Flask, request, jsonify
 from groq import Groq
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = Flask(__name__)
-
-api_key = os.getenv("GROQ_API_KEY")
-
-client = Groq(
-    api_key=api_key
-)
 
 
 @app.route("/")
@@ -21,25 +12,38 @@ def home():
 
 @app.route("/chat", methods=["POST"])
 def chat():
+
     try:
+
+        api_key = os.environ.get("GROQ_API_KEY")
+
+        if not api_key:
+            return jsonify({
+                "error": "GROQ_API_KEY is missing in Vercel"
+            }), 500
+
+
+        client = Groq(
+            api_key=api_key
+        )
+
+
         data = request.get_json()
 
         message = data.get("message")
 
-        if not message:
-            return jsonify({
-                "error": "No message received"
-            }), 400
-
 
         response = client.chat.completions.create(
+
             model="llama-3.1-8b-instant",
+
             messages=[
                 {
                     "role": "user",
                     "content": message
                 }
             ]
+
         )
 
 
@@ -49,8 +53,6 @@ def chat():
 
 
     except Exception as e:
-
-        print("ERROR:", str(e))
 
         return jsonify({
             "error": str(e)
